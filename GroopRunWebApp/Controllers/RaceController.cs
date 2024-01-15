600110ee31585a8d33e4f6ecff4130584a3abb95
thasocheat <thasocheat167@gmail.com>
2024-01-15 14:12:13 +0700

    update on club and race list data

--- GroopRunWebApp/Controllers/RaceController.cs
+++ GroopRunWebApp/Controllers/RaceController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GroopRunWebApp.Data;
+using GroopRunWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GroopRunWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> Races = _context.Races.ToList();
+            return View(Races);
         }
     }
 }
